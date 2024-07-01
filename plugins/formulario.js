4b6932feeb20fa596ae163dbc9092a6841bb4dc5
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-01 18:40:51 -0500

    Update formulario.js

--- plugins/formulario.js
+++ plugins/formulario.js
@@ -4,15 +4,15 @@ const handler = async (m, {conn}) => {
 handler.command = /^(Ficha)$/i;
 export default handler;
 global.Ficha = `
-*LLENAR FORMULARIO*
+LLENAR FORMULARIO
 
-*NICK FF :*
-*NICK PERSONAL :*
-*EDAD :*
-*DISPONIBILIDAD DE TIEMPO :*
-*EXP EN VV2 :*
-*EXP EN MAPA :*
+NICK FF :
+NICK PERSONAL :
+EDAD :
+DISPONIBILIDAD DE TIEMPO :
+EXP EN COMPE :
+EXP EN MAPA :
 
 
-*Nota : Recuerda Una Vez Hayas Pasado El Formulario Se Te Hara Prueba En Vv2 Si Mandas Ig O Tus Clips De Ahora Entras Directo*
+Nota : Recuerda Una Vez Hayas Pasado El Formulario Se Te Hara Prueba En compe y mapa grande Si Mandas Ig O Tus Clips De Ahora Entras Directo
 `;
