ec71dbab7d8327766b62415e22efa164764220db
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-01 21:14:01 -0500

    Update semigey.js

--- plugins/semigey.js
+++ plugins/semigey.js
@@ -1,4 +1,8 @@
 const handler = async (m, {conn}) => {
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+const handler = async (m, {conn}) => {
   m.reply(global.semigey);
 };
 handler.command = /^(semigey|semigay)$/i;
