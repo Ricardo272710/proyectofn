79cbd1f039ec49d3f7e45c0a52246ae878a6e482
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-01 21:07:32 -0500

    Create semigey.js

---
+++ plugins/semigey.js
@@ -0,0 +1,13 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.semigey);
+};
+handler.command = /^(semigey|semigay)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.semigey = `
+ohhhh si soy gay me gusta el 
+`;
+`;
