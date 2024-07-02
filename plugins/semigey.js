7b3020dbb2889ca6fec92fa2300668ce70c0a77f
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-01 21:24:01 -0500

    Update semigey.js

--- plugins/semigey.js
+++ plugins/semigey.js
@@ -7,7 +7,9 @@ export default handler;
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
-
+let ff = `https://telegra.ph/file/24fa902ead26340f3df2c.png`
+await conn.sendFile(m.chat, 'https://telegra.ph/file/24fa902ead26340f3df2c.png', 'fantasy.jpg', semigaytxt.trim(),
+                    
 global.semigey = `
 Soy semi y si soy gay me gusta el pene negro
 `;
